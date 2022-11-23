b0da2c6d7effc19d3925b23a44542399d48d2a84
“joaobritus” <joaoaragaobrito@gmail.com>
2022-11-23 14:39:30 +0000

    yoyoyo

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaragao- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jaragao- <jaragao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 13:50:39 by jaragao-          #+#    #+#             */
-/*   Updated: 2022/11/08 14:34:14 by jaragao-         ###   ########.fr       */
+/*   Updated: 2022/11/22 16:25:17 by jaragao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
 
 char	*ft_strdup(const char *s)
 {
